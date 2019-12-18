0e5715999b680d6b9404efff105992fa961320de
Humberto Leal <humberto.adolfo@hotmail.com>
2019-12-18 15:24:01 -0500

    Original code adapted

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,105 @@
+//Include LCD and I2C library
 #include <Arduino.h>
-#include "IMUSensor/IMUSensor.h"
+#include <MPU9255.h>
 
-IMUSensor imu(SDA, SCL, scale_16g, scale_500dps, 0.06);
-float angle_pitch = 0;
-float angle_roll = 0;
+//Declaring some global variables
+int gyro_x, gyro_y, gyro_z;
+long acc_x, acc_y, acc_z, acc_total_vector;
+int temperature;
+long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+long loop_timer;
+int lcd_loop_counter;
+float angle_pitch, angle_roll;
+int angle_pitch_buffer, angle_roll_buffer;
+boolean set_gyro_angles;
+float angle_roll_acc, angle_pitch_acc;
+float angle_pitch_output, angle_roll_output;
 
-double angle_pitch_acc;
-double angle_roll_acc;
+//Initialize the LCD library
+MPU9255 mpu;
 
-double angle_pitch_output;
-double angle_roll_output;
 
-long acc_total_vector;
-bool set_gyro_angles = false;
+void read_mpu_6050_data();
 
-void setup()
-{
-    // put your setup code here, to run once:
+void setup() {
     Serial.begin(9600);
-    imu.begin();
+    
+    if (mpu.init()) {
+        Serial.println("NO Inicializado");
+    }
+    else Serial.println("inicializado");
+  
+  for (int cal_int = 0; cal_int < 2000 ; cal_int ++){                  //Run this code 2000 times
+    read_mpu_6050_data();                                              //Read the raw acc and gyro data from the MPU-6050
+    gyro_x_cal += gyro_x;                                              //Add the gyro x-axis offset to the gyro_x_cal variable
+    gyro_y_cal += gyro_y;                                              //Add the gyro y-axis offset to the gyro_y_cal variable
+    gyro_z_cal += gyro_z;                                              //Add the gyro z-axis offset to the gyro_z_cal variable
+    delay(3);                                                          //Delay 3us to simulate the 250Hz program loop
+  }
+  gyro_x_cal /= 2000;                                                  //Divide the gyro_x_cal variable by 2000 to get the avarage offset
+  gyro_y_cal /= 2000;                                                  //Divide the gyro_y_cal variable by 2000 to get the avarage offset
+  gyro_z_cal /= 2000;                                                  //Divide the gyro_z_cal variable by 2000 to get the avarage offset                                          //All done, turn the LED off
+  
+  loop_timer = micros();                                               //Reset the loop timer
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
-    Axis<int16_t> rotation_rate = imu.get_gyroscope_readings();
-    Axis<int16_t> accel = imu.get_accelerometer_readings();
-    
-    Axis<double> acceleration = imu.get_acceleration();
-    //Serial.println(acceleration.z);
-    
-    
-    //Serial.println(rotation_rate.x);
-    // (1 / 250)/ 131
-    // 131 from datasheet
-    // 250hz from frequency of sensor readings. (4ms period)
-    // angle_pitch += rotation_rate.x * 0.0000305;
-    // angle_roll += rotation_rate.y * 0.0000305;
-    angle_pitch += rotation_rate.x * 0.0000611;
-    angle_roll += rotation_rate.y * 0.0000611;
-    
-    //Serial.print("pitch, x: "); Serial.print(angle_pitch); Serial.print(" roll, y: "); Serial.println(angle_roll);
-    
-    angle_pitch += angle_roll * sin(rotation_rate.z * 0.000001066);
-    angle_roll -= angle_pitch * sin(rotation_rate.z * 0.000001066);
-    
-    Serial.print("pitch, x: "); Serial.print(angle_pitch); Serial.print(" roll, y: "); Serial.println(angle_roll);
-    
-    acc_total_vector = sqrt((acceleration.x*acceleration.x)+(acceleration.y*acceleration.y)+(acceleration.z*acceleration.z));
-    
-    angle_pitch_acc = (asin(acceleration.y / acc_total_vector) ? asin(acceleration.y/acc_total_vector) : 90.0)*57.296;
-    angle_roll_acc = asin(acceleration.x/acc_total_vector)* -57.296;
-    
-    //Serial.print("pitch, x: "); Serial.print(angle_pitch_acc); Serial.print(" roll, y: "); Serial.println(angle_roll_acc);
-    
-    // if(set_gyro_angles) {
-    //     angle_pitch = angle_pitch *0.9996 + angle_pitch_acc * 0.0004;
-    //     angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;
-    // }
-    // else {
-    //     angle_pitch = angle_pitch_acc;
-    //     angle_roll = angle_roll_acc;
-    //     set_gyro_angles = true;
-    // }
-    
-    // //Serial.print("pitch, x: "); Serial.print(angle_pitch); Serial.print(" roll, y: "); Serial.println(angle_roll);
-    
-    // angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;
-    // angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;
-    
-    // //Serial.print("pitch, x: "); Serial.print(angle_pitch_output); Serial.print(" roll, y: "); Serial.println(angle_roll_output);
-    
-    delay(4);
+void loop(){
+
+  read_mpu_6050_data();                                                //Read the raw acc and gyro data from the MPU-6050
+
+  gyro_x -= gyro_x_cal;                                                //Subtract the offset calibration value from the raw gyro_x value
+  gyro_y -= gyro_y_cal;                                                //Subtract the offset calibration value from the raw gyro_y value
+  gyro_z -= gyro_z_cal;                                                //Subtract the offset calibration value from the raw gyro_z value
+  
+  //Gyro angle calculations
+  //0.0000611 = 1 / (250Hz / 65.5)
+  angle_pitch += gyro_x * 0.0000611;                                   //Calculate the traveled pitch angle and add this to the angle_pitch variable
+  angle_roll += gyro_y * 0.0000611;                                    //Calculate the traveled roll angle and add this to the angle_roll variable
+  //Serial.print("Pitch x: "); Serial.print(angle_pitch); Serial.print(" Roll y: "); Serial.println(angle_roll);
+  //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
+  angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the roll angle to the pitch angel
+  angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the pitch angle to the roll angel
+  //Serial.print("Pitch x: "); Serial.print(angle_pitch); Serial.print(" Roll y: "); Serial.println(angle_roll);
+  //Accelerometer angle calculations
+  acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));  //Calculate the total accelerometer vector
+  //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+  angle_pitch_acc = asin((float)acc_y/acc_total_vector)* 57.296;       //Calculate the pitch angle
+  angle_roll_acc = asin((float)acc_x/acc_total_vector)* -57.296;       //Calculate the roll angle
+  
+  Serial.print("Pitch x: "); Serial.print(angle_pitch_acc); Serial.print(" Roll y: "); Serial.println(angle_roll_acc);
+  //Place the MPU-6050 spirit level and note the values in the following two lines for calibration
+  angle_pitch_acc -= 0.0;                                              //Accelerometer calibration value for pitch
+  angle_roll_acc -= 0.0;                                               //Accelerometer calibration value for roll
+
+  if(set_gyro_angles){                                                 //If the IMU is already started
+    angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;     //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
+    angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;        //Correct the drift of the gyro roll angle with the accelerometer roll angle
+  }
+  else{                                                                //At first start
+    angle_pitch = angle_pitch_acc;                                     //Set the gyro pitch angle equal to the accelerometer pitch angle 
+    angle_roll = angle_roll_acc;                                       //Set the gyro roll angle equal to the accelerometer roll angle 
+    set_gyro_angles = true;                                            //Set the IMU started flag
+  }
+  
+  //To dampen the pitch and roll angles a complementary filter is used
+  angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;   //Take 90% of the output pitch value and add 10% of the raw pitch value
+  angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;      //Take 90% of the output roll value and add 10% of the raw roll value
+                                                    //Write the roll and pitch values to the LCD display
+    //Serial.print("Pitch x: "); Serial.print(angle_pitch_output); Serial.print(" Roll y: "); Serial.println(angle_roll_output);
+  while(micros() - loop_timer < 4000);                                 //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+  loop_timer = micros();                                               //Reset the loop timer
+}
+
+
+void read_mpu_6050_data(){                                             //Subroutine for reading the raw gyro and accelerometer data
+    mpu.set_gyro_scale(scale_500dps);
+  mpu.read_acc();
+  acc_x = mpu.ax;
+  acc_y = mpu.ay;
+  acc_z = mpu.az;
+    mpu.read_gyro();
+  gyro_x = mpu.gx;
+  gyro_y = mpu.gy;
+  gyro_z = mpu.gz;
+
 }
